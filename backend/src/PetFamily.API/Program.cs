c989451881b2105848f61ce2d01ed57430a18689
omintbp <zhiltcov.ds@gmail.com>
2024-08-31 13:17:04 +1000

    B-5-4 Add logging for creating a volunteer

--- backend/src/PetFamily.API/Program.cs
+++ backend/src/PetFamily.API/Program.cs
@@ -1,13 +1,30 @@
 using PetFamily.API;
 using PetFamily.Application;
 using PetFamily.Infrastructure;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.Debug()
+    .WriteTo.Seq(builder.Configuration.GetConnectionString("Seq") 
+                 ?? throw new ArgumentNullException("Seq connection string is missing"))
+    .Enrich.WithThreadId()  
+    .Enrich.WithEnvironmentName()
+    .Enrich.WithEnvironmentUserName()
+    .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Mvc", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Routing", LogEventLevel.Warning)
+    .CreateLogger();
 
 builder.Services
     .AddApi()
     .AddInfrastructure()
     .AddApplication();
+
+builder.Services.AddSerilog();
 
 var app = builder.Build();
 
@@ -16,6 +33,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 app.MapControllers();
