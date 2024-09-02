f827f0380e5322ffff5321520751f27926d67ddc
omintbp <zhiltcov.ds@gmail.com>
2024-09-02 15:58:10 +1000

    B-7-1 Add Minio provider

---
+++ backend/src/PetFamily.Application/Files/Delete/DeleteFileRequest.cs
@@ -0,0 +1,3 @@
+namespace PetFamily.Application.Files.Delete;
+
+public record DeleteFileRequest(string ObjectName, string BucketName);
--- backend/src/PetFamily.Infrastructure/Inject.cs
+++ backend/src/PetFamily.Infrastructure/Inject.cs
@@ -1,17 +1,44 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PetFamily.Application.Providers;
 using PetFamily.Application.Volunteers;
+using PetFamily.Infrastructure.Options;
+using PetFamily.Infrastructure.Providers;
 using PetFamily.Infrastructure.Repositories;
+using Minio;
 
 namespace PetFamily.Infrastructure;
 
 public static class Inject
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddScoped<ApplicationDbContext>();
 
         services.AddScoped<IVolunteerRepository, VolunteerRepository>();
-        
+
+        services.AddMinio(configuration);
+
+        return services;
+    }
+
+    private static IServiceCollection AddMinio(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<MinioOptions>(configuration.GetSection(MinioOptions.MINIO));
+
+        services.AddMinio(options =>
+        {
+            var minioOptions = configuration.GetSection(MinioOptions.MINIO).Get<MinioOptions>()
+                               ?? throw new ApplicationException("Missing minio configuration");
+
+            options.WithEndpoint(minioOptions.Endpoint);
+
+            options.WithCredentials(minioOptions.AccessKey, minioOptions.SecretKey);
+            options.WithSSL(minioOptions.WithSsl);
+        });
+
+        services.AddScoped<IFileProvider, MinioProvider>();
+
         return services;
     }
 }
